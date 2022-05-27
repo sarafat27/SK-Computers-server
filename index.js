50ac33b9a10a13f246519d8874cd44d3b8108166
sarafat27 <sarafat2769@gmail.com>
2022-05-27 19:58:02 +0600

    payment completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 //middleware
 app.use(cors())
@@ -37,6 +38,7 @@ async function run() {
         const orderCollection = client.db('sk_computers').collection('orders')
         const reviewCollection = client.db('sk_computers').collection('reviews')
         const profileCollection = client.db('sk_computers').collection('profiles')
+        const paymentCollection = client.db('sk_computers').collection('payments')
 
         const verifyAdmin = async (req, res, next) => {
             const requester = req.decoded.email;
@@ -189,6 +191,32 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const order = await orderCollection.findOne(query)
             res.send(order)
+        });
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const { totalPrice } = req.body;
+            const amount = totalPrice * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+        //add paid property to an order when paid
+        app.patch('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const result = await paymentCollection.insertOne(payment);
+            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedOrder)
         })
 
     }
